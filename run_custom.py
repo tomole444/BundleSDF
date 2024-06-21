669596ece3524b227bfc7297298627213c62901d
Tommy L <tom.leyh@study.thws.de>
2024-06-21 11:46:10 +0200

    id_str fix

--- run_custom.py
+++ run_custom.py
@@ -81,7 +81,7 @@ def run_one_video(video_dir='/home/bowen/debug/2022-11-18-15-10-24_milk', out_fo
   tracker.time_keeper.add("whole_runtime", 0)
 
   for i in range(0,len(reader.color_files),args.stride):
-    tracker.time_keeper.add("preprocessing", int(id_str))
+    tracker.time_keeper.add("preprocessing", int(reader.id_strs[i]))
     color_file = reader.color_files[i]
     color = cv2.imread(color_file)
     H0, W0 = color.shape[:2]
