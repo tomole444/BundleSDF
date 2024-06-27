355bc7ae9ce7c1622ab8cbc729357623c80264cd
Tommy L <tom.leyh@study.thws.de>
2024-06-27 15:38:11 +0200

    better condition readability

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -314,7 +314,7 @@ class BenchmarkSegmentation:
 
 
 def calcADD():
-    bench = BenchmarkADD(pose_pred_dir="/home/thws_robotik/Documents/Leyh/6dpose/detection/BundleSDF/outBuchVideoPVNetSegOnly/ob_in_cam",
+    bench = BenchmarkADD(pose_pred_dir="/home/thws_robotik/Documents/Leyh/6dpose/detection/BundleSDF/outBuchVideoFirstMaskPVNet/ob_in_cam",
                       pose_gt_dir= "/home/thws_robotik/Documents/Leyh/6dpose/datasets/BuchVideo/pose",
                       model_path="/home/thws_robotik/Documents/Leyh/6dpose/datasets/BuchVideo/model.ply",
                       model_diameter=0.211,
@@ -322,7 +322,7 @@ def calcADD():
     bench.run_add_pose()
     #bench.run_occlusion()
     bench.plot_results()
-    bench.save_results("benchmarks/BuchVideo/ADD_BundleSDF_pvnet_segmentation_only.npy")
+    bench.save_results("benchmarks/BuchVideo/ADD_Test.npy")
 
 def calcMaskMetrics():
     bench = BenchmarkSegmentation(masks_est_dir= "/home/thws_robotik/Documents/Leyh/6dpose/datasets/BuchVideo/masks_xmem_first_pvnet",
@@ -333,5 +333,5 @@ def calcMaskMetrics():
 
 
 if __name__ == "__main__":
-    #calcADD()
-    calcMaskMetrics()
+    calcADD()
+    #calcMaskMetrics()
--- plot_generic.py
+++ plot_generic.py
@@ -275,11 +275,12 @@ class ResultPlotter:
         #plt.plot(self.x,self.add_bundle_feature_matching_spike, label = "ADD feature spike prevention")
         #plt.plot(self.x,self.add_bundle_pose_regression, label = "ADD Pose regression")
         #plt.plot(self.x,self.add_bundle_pose_regression, label = "ADD Pose regression 2")
-        #plt.plot(self.x,self.add_bundle_cutie_first_offline_segmentation, label = "ADD Cutie segmentation")
+        plt.plot(self.x,self.add_bundle_cutie_first_offline_segmentation, label = "ADD First Offline Cutie segmentation")
         #plt.plot(self.x,self.add_bundle_orig_cutie_segmentation, label = "ADD Orig Cutie segmentation")
         #plt.plot(self.x,self.add_bundle_orig_xmem_segmentation, label = "ADD Orig XMem segmentation")
         plt.plot(self.x,self.add_bundle_first_pvnet_cutie_segmentation, label = "ADD First PVNet Cutie Segmentation")
-        plt.plot(self.x,self.add_bundle_pvnet_seg_only, label = "ADD PVNet Only Segmentation")
+        #plt.plot(self.x,self.add_bundle_pvnet_seg_only, label = "ADD PVNet Only Segmentation")
+        plt.plot(self.x,self.add_test, label = "ADD First PVNet Cutie Segmentation_2")
 
 
 
